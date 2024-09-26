4b68b06bd8ac69c5d777e4290cc6b4d985dc83f3
inunice <ammanguan@up.edu.ph>
2024-09-26 21:10:49 +0800

    feat: scrape basic elements from a work

--- app/api/getBasicInfo/route.tsx
+++ app/api/getBasicInfo/route.tsx
@@ -1,10 +1,49 @@
+import { Cheerio } from "cheerio";
 import { NextRequest, NextResponse } from "next/server";
+
+const cheerio = require("cheerio");
+
+function getTextArray($: any, selector: string) {
+  return $(selector)
+    .map((i: number, el: any) => {
+      return $(el).text();
+    })
+    .get();
+}
 
 export async function GET(req: NextRequest) {
   try {
     const response = await fetch("https://archiveofourown.org/works/26337703");
-    const text = await response.text();
-    return new NextResponse(text, { status: 200 });
+    const htmlString = await response.text();
+    const $ = cheerio.load(htmlString);
+
+    const title = $("h2.title").text().trim();
+    const author = $('a[rel="author"]').text();
+    const summary = $("div.summary p")
+      .map((i: any, el: any) => $(el).text())
+      .get()
+      .join("\n");
+
+    const fandom = $("dd.fandom").text();
+    const relationships = getTextArray($, "dd.relationship ul li");
+    const characters = getTextArray($, "dd.character ul li");
+
+    const language = $("dd.language").text();
+
+    return new NextResponse(
+      JSON.stringify({
+        relationships,
+        characters,
+        fandom,
+        title,
+        author,
+        summary,
+        language,
+      }),
+      {
+        status: 200,
+      }
+    );
   } catch (error) {
     return new NextResponse(JSON.stringify({ error: "Failed to fetch data" }), {
       status: 500,
