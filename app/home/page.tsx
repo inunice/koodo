b3beb4cc4b0f5c04341b6c11e186f2744201f3e9
inunice <ammanguan@up.edu.ph>
2024-09-29 03:19:05 +0800

    refactor: move add work form to component

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,44 +1,19 @@
 "use client";
 
 import { useState } from "react";
-import supabase from "@/config/supabaseClient";
 
 import { saveWork } from "@/app/api/saveWork";
 
 import { WorkInfo } from "@/types/workInfo";
 
 import WorkCard from "./workCard";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import WorkForm from "./workForm";
 
 export default function Home() {
-  const [link, setLink] = useState("");
-  const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const [work, setWork] = useState<WorkInfo | null>(null);
 
-  const getWorkInformation = async () => {
-    try {
-      if (!link.startsWith("https://archiveofourown.org/works/")) {
-        throw new Error(
-          "Please copy the full link from the work page starting at the first chapter."
-        );
-      }
-
-      const url = new URL(link);
-      const id = url.pathname.split("/")[2];
-
-      const response = await fetch(
-        `/api/getWorkInfo?id=${encodeURIComponent(id)}`,
-        {
-          method: "GET",
-        }
-      );
-      const work = await response.json();
-      setWork(work);
-      setErrorMessage(null);
-    } catch (error: any) {
-      console.error("Error fetching data:", error);
-      setErrorMessage(error.message);
-    }
+  const getWorkInformation = (work: WorkInfo) => {
+    setWork(work);
   };
 
   const handleSaveWork = () => {
@@ -50,21 +25,7 @@ export default function Home() {
   return (
     <div>
       <h1>Enter Fic URL</h1>
-      <div>
-        <input
-          type="text"
-          value={link}
-          onChange={(e) => setLink(e.target.value)}
-          placeholder="Enter link"
-        />
-        <button onClick={getWorkInformation}>Get work info</button>
-      </div>
-      {errorMessage && (
-        <Alert variant="destructive">
-          <AlertTitle>Invalid link!</AlertTitle>
-          <AlertDescription>{errorMessage}</AlertDescription>
-        </Alert>
-      )}
+      <WorkForm setWorkInfo={getWorkInformation} />
       {work && <WorkCard work={work} />}
       <button onClick={handleSaveWork}>Add work</button>
     </div>
---
+++ app/home/workForm.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useState } from "react";
+
+import { WorkInfo } from "@/types/workInfo";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
+export default function WorkForm({
+  setWorkInfo,
+}: {
+  setWorkInfo: (work: WorkInfo) => void;
+}) {
+  const [link, setLink] = useState("");
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const getWorkInformation = async () => {
+    try {
+      if (!link.startsWith("https://archiveofourown.org/works/")) {
+        throw new Error(
+          "Please copy the full link from the work page starting at the first chapter."
+        );
+      }
+
+      const url = new URL(link);
+      const id = url.pathname.split("/")[2];
+
+      const response = await fetch(
+        `/api/getWorkInfo?id=${encodeURIComponent(id)}`,
+        {
+          method: "GET",
+        }
+      );
+      const work = await response.json();
+      setWorkInfo(work);
+      setErrorMessage(null);
+    } catch (error: any) {
+      console.error("Error fetching data:", error);
+      setErrorMessage(error.message);
+    }
+  };
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          value={link}
+          onChange={(e) => setLink(e.target.value)}
+          placeholder="Enter link"
+        />
+        <button onClick={getWorkInformation}>Get work info</button>
+      </div>
+      {errorMessage && (
+        <Alert variant="destructive">
+          <AlertTitle>Invalid link!</AlertTitle>
+          <AlertDescription>{errorMessage}</AlertDescription>
+        </Alert>
+      )}
+    </div>
+  );
+}
