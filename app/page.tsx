3c9089a22ac26c663e3f5ae678bd7993e96820d7
inunice <ammanguan@up.edu.ph>
2024-09-29 00:07:24 +0800

    deps: set up supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import Image from "next/image";
+import supabase from "@/config/supabaseClient";
 
 export default function Home() {
+  console.log(supabase);
   return (
     <div>
       <span>koodo</span>
