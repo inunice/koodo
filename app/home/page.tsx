8b81558fde5131e0ef5c88b075b29fb3181d1c59
inunice <ammanguan@up.edu.ph>
2024-09-28 23:00:21 +0800

    feat: ui for invalid link error

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import { useState } from "react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 
 export default function Home() {
   const [link, setLink] = useState("");
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   const getWorkInformation = async () => {
     try {
       if (!link.startsWith("https://archiveofourown.org/works/")) {
         throw new Error(
-          "Invalid link. Please copy the full link from the work page starting at the first chapter."
+          "Please copy the full link from the work page starting at the first chapter."
         );
       }
 
@@ -24,8 +26,10 @@ export default function Home() {
       );
       const data = await response.json();
       console.log(data);
-    } catch (error) {
+      setErrorMessage(null);
+    } catch (error: any) {
       console.error("Error fetching data:", error);
+      setErrorMessage(error.message);
     }
   };
 
@@ -41,6 +45,12 @@ export default function Home() {
         />
         <button onClick={getWorkInformation}>Get work info</button>
       </div>
+      {errorMessage && (
+        <Alert variant="destructive">
+          <AlertTitle>Invalid link!</AlertTitle>
+          <AlertDescription>{errorMessage}</AlertDescription>
+        </Alert>
+      )}
     </div>
   );
 }
