fa0a26cf1e7ebea02ac973c277c0c281a176a68c
inunice <ammanguan@up.edu.ph>
2024-09-29 22:13:57 +0800

    feat: bit of styling on home page

--- app/home/bookmarkCard.tsx
+++ app/home/bookmarkCard.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 
 import { Bookmark } from "@/types/bookmarkInfo";
 import { Card } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
 import {
   Dialog,
   DialogTitle,
@@ -20,17 +21,21 @@ export default function WorkCard({ bookmark }: BookmarkCardProps) {
   return (
     <Dialog>
       <DialogTrigger>
-        <Card className="flex flex-col p-3">
+        <Card className="flex flex-col items-start p-3">
           <p className="text-md">{bookmark.workDetails?.workBasicInfo.title}</p>
           <p className="text-sm text-gray-700">
             {bookmark.workDetails?.workBasicInfo.author}
           </p>
-          <p className="text-xs text-gray-700">
-            {bookmark.workDetails?.workTags.fandoms}
-          </p>
+          <div className="flex flex-wrap gap-1">
+            {bookmark.workDetails?.workTags.fandoms.map((fandom, index) => (
+              <Badge key={index} variant="outline" className="text-[10px]">
+                {fandom}
+              </Badge>
+            ))}
+          </div>
 
           <p>{bookmark.userBookmarkDetails.status}</p>
-          <div className="flex justify-end">
+          <div className="flex">
             {bookmark.workDetails && (
               <Link href={bookmark.workDetails.workLink}>x</Link>
             )}
@@ -39,7 +44,10 @@ export default function WorkCard({ bookmark }: BookmarkCardProps) {
       </DialogTrigger>
       <DialogContent>
         <DialogTitle>{bookmark.workDetails?.workBasicInfo.title}</DialogTitle>
-        <DialogDescription>Yes</DialogDescription>
+        <DialogDescription>
+          <p>{bookmark.workDetails?.workBasicInfo.author}</p>
+          <p>{bookmark.workDetails?.workBasicInfo.summary}</p>
+        </DialogDescription>
       </DialogContent>
     </Dialog>
   );
