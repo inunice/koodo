161a6fac88bd8023e9a95b84d0b77b73f77cf9b3
inunice <ammanguan@up.edu.ph>
2024-10-05 19:51:49 +0800

    feat: information on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,52 @@
+"use client";
+
+import { useBookmarks } from "@/context/bookmarkContext";
+import { Bookmark } from "@/types/bookmarkInfo";
+
 export default function Home() {
+  const { bookmarks } = useBookmarks();
+
+  const readingBookmarks = bookmarks.filter(
+    (bookmark) => bookmark.readingStatus === "Reading"
+  );
+
+  const bookmarksByTag = bookmarks.reduce<Record<string, Bookmark[]>>(
+    (acc, bookmark) => {
+      bookmark.mainTags.forEach((tag) => {
+        if (!acc[tag]) {
+          acc[tag] = [];
+        }
+        acc[tag].push(bookmark);
+      });
+      return acc;
+    },
+    {}
+  );
+
   return (
     <main>
-      <h1>Home</h1>
+      <div className="mb-10">
+        <h2>Currently Reading</h2>
+        <ul>
+          {readingBookmarks.map((bookmark) => (
+            <li key={bookmark.workID}>{bookmark.workBasicInfo.title}</li>
+          ))}
+        </ul>
+      </div>
+
+      <h2>By Tags</h2>
+      {Object.keys(bookmarksByTag).map((tag) => (
+        <div key={tag} className="mb-5">
+          <h3 className="font-bold">{tag}</h3>
+          <ul>
+            {bookmarksByTag[tag].map((bookmark) => (
+              <li key={bookmark.workID}>
+                {bookmark.workBasicInfo.title} - {bookmark.readingStatus}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </main>
   );
 }
