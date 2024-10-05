61ca2ea5020ce9496c4319eaa160c8014d0adab7
inunice <ammanguan@up.edu.ph>
2024-10-05 14:47:04 +0800

    fix: settings dynamic import

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,5 +1,7 @@
-import ExportBackup from "./exportBackup";
-import ImportBackup from "./importBackup";
+import dynamic from "next/dynamic";
+
+const ExportBackup = dynamic(() => import("./exportBackup"), { ssr: false });
+const ImportBackup = dynamic(() => import("./importBackup"), { ssr: false });
 
 export default function SettingsPage() {
   return (
