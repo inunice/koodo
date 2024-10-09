cfa2edab2afffa384abb62e2eafe26f143156a27
inunice <ammanguan@up.edu.ph>
2024-10-09 18:43:05 +0800

    feat: progress on reading card

--- app/components/currently-reading-card.tsx
+++ app/components/currently-reading-card.tsx
@@ -24,10 +24,16 @@ export default function CurrentlyReadingCard({
     router.push(`/bookmarks/${bookmark.workID}`);
   };
 
+  const latestChapter = bookmark.workDetails?.workStats?.latestChapter || 1;
+  const progressPercentage =
+    (bookmark.currentChapter / latestChapter || 1) * 100;
+  const words = bookmark.workDetails?.workStats?.words || 1;
+  const estimatedWordsLeft = words * (1 - progressPercentage / 100);
+
   return (
     <>
       <div onClick={handleNavigate}>
-        <Card className="w-[270px] h-[150px] flex flex-col items-start px-6 py-6 gap-2 align-left">
+        <Card className="w-[270px] h-[170px] flex flex-col items-start px-6 py-6 gap-2 align-left">
           <CardHeader className="p-0">
             <div className="items-baseline flex flex-wrap gap-1">
               {bookmark.favorite && <HeartFilled className="w-3 h-3" />}
@@ -39,12 +45,22 @@ export default function CurrentlyReadingCard({
               </span>
             </div>
           </CardHeader>
-          <CardContent className="flex flex-col gap-1 w-full p-0">
+          <CardContent className="flex flex-col gap-1 w-full p-0 flex-grow">
             <p className="text-xs text-justify line-clamp-3 text-gray-500">
               {bookmark.workBasicInfo.summary.join(" ")}
             </p>
           </CardContent>
-          <CardFooter className="w-full flex flex-row p-0 pt-2 gap-10 justify-between"></CardFooter>
+          <CardFooter className="w-full flex flex-row p-0 pt-2 gap-10 justify-between text-xs">
+            <div>
+              <span>
+                {bookmark.currentChapter} /{" "}
+                {bookmark.workDetails?.workStats.latestChapter}
+              </span>
+            </div>
+            <span>
+              {Math.round(estimatedWordsLeft).toLocaleString()} words left
+            </span>
+          </CardFooter>
         </Card>
       </div>
     </>
